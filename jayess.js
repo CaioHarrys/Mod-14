f86975fdae07c3bff38c7aeeed8d4057513bcc14
Harrys <blackiaratanmusic@gmail.com>
2022-07-08 16:31:09 -0300

    FINALIZADO

--- jayess.js
+++ jayess.js
@@ -1,37 +1,50 @@
-function vascoM() {
+  function validate(){
     var nome = document.getElementById("nome").value;
-    if (nome==null || nome=="") {
-        alert("Escreva um Nome valido.");
-        console.log("Campo de Nome preenchido incorretamente.");
-    } else{
-        console.log("Campo de Nome preenchido com sucesso!");
-        }
     var sobrenome = document.getElementById("sobrenome").value;
-    if (sobrenome==null || sobrenome=="") {
-        alert("Escreva um Sobrenome valido");
-        console.log("Campo de Sobrenome preenchido incorretamente.");
-    } else {
-        console.log("Campo de Sobrenome preenchido corretamente!");
-    }
     var idNumero = document.getElementById("idNumero").value;
-    if (idNumero.length < 11 || idNumero.length > 11 ||isNaN(idNumero)) {
-        alert("Digite um CPF Valido");
-        console.log("Campo CPF preenchido incorretamente.");
-    } else {
-            console.log("Campo CPF preenchido corretamente!");
-        }
-      var DD = document.getElementById("DD").value;
-      if (isNaN(DD) || DD.length < 2 || DD.length > 2 ||DD==null || DD=="") {
-          alert("Digite um DD Valido");
-          console.log("Campo DD preenchido incorretamente.");
-        } else {
-            console.log("Campo DD preenchido corretamente!");
-        }
+    var DD = document.getElementById("DD").value;
     var phone = document.getElementById("phone").value;
-    if (isNaN(phone) ||phone==null || phone==""|| phone <= 0 || phone.length < 9 || phone.length > 9) {
-        alert("Digite um Telefone valido");
-        console.log("Campo de Telefone preenchido incorretamente.");
-      } else {
-        console.log("Campo de Telefone Preenchido Corretamente!");
-      }
+    var error_message = document.getElementById("error_message");
+    
+    error_message.style.padding = "10px";
+    
+    var text;
+    if(nome==null || nome==""){
+      text = "Please Enter valid Name";
+      error_message.innerHTML = text;
+      document.getElementById('error_message').classList.remove('hide') = text;
+      return false;
+    }
+    if(sobrenome==null || sobrenome==""){
+      text = "Please Enter valid Last Name";
+      error_message.innerHTML = text;
+      document.getElementById('error_message').classList.remove('hide') = text;
+      return false;
+    }
+    if(idNumero.length < 11 || idNumero.length > 11 ||isNaN(idNumero)){
+      text = "Please Enter valid ID Number";
+      error_message.innerHTML = text;
+      document.getElementById('error_message').classList.remove('hide') = text;
+      return false;
+    }
+    if(isNaN(DD) || DD.length < 2 || DD.length > 2 ||DD==null || DD==""){
+      text = "Please Enter valid Area Number";
+      error_message.innerHTML = text;
+      document.getElementById('error_message').classList.remove('hide') = text;
+      return false;
+    }
+    if(isNaN(phone) || phone.length < 9 || phone.length > 9 ||phone==null || phone==""){
+      text = "Please Enter Valid Phone Number";
+      error_message.innerHTML = text;
+      document.getElementById('error_message').classList.remove('hide') = text;
+      return false;
+    }
+    alert("Form Submitted Successfully!");
+    document.getElementById('error_message').classList.add('hide');
+    var nome = document.getElementById("nome").value="";
+    var sobrenome = document.getElementById("sobrenome").value="";
+    var idNumero = document.getElementById("idNumero").value="";
+    var DD = document.getElementById("DD").value="";
+    var phone = document.getElementById("phone").value="";
+    return true;
   }
